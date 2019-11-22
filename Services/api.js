6b7cd87b45890009aa9ca08fa1d8f92971a55371
Helio Silva <suporte.computek@gmail.com>
2019-11-22 10:13:24 -0200

    Adicionado ip da AWS

--- Services/api.js
+++ Services/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios' ;
 
-const trabalho = true ;
+const local = false ;
 
 const api = axios.create({
     
-    baseURL:  (trabalho) ? 'http://10.0.0.102:4545'    :
-                           'http://192.168.0.104:4545' ,
+    baseURL:  (local) ? 'http://10.0.0.102:4545'    :
+                           'http://3.90.37.26:4545' ,
 });
 
 
